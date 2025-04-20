2b7b83a1f7c754d4117b43affb1418b219c7e537
Daniel Ruggeri <DRuggeri@primary.net>
2025-04-20 11:20:00 -0500

    Tidy up links and create some reusable functions

---
+++ site/js/boss.js
@@ -0,0 +1,31 @@
+async function backgroundSend(target) {
+    return await fetch(target)
+        .then(response => {
+            if (!response.ok) {
+                return false
+            }
+            return true
+        })
+        .catch(error => {
+            console.log(error)
+            return false
+        }
+    )
+}
+
+function gotoSlide(target) {
+    const slide = Reveal.getSlides().find( ( slide ) => {
+        return slide.id == target
+    });
+    if (slide ) {
+        i = Reveal.getIndices( slide )
+        Reveal.slide(i.h, i.v, i.f)
+    } else {
+        console.log(`could not find target slide`)
+    }
+}
+
+function navigateBottomWindow(url) {
+    const target = encodeURIComponent(url)
+    backgroundSend(`http://boss.local:8080/navigate?url=${target}`)
+}
