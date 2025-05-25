052759c5969a9adc693a5c05e40151f6b4acd972
Daniel Ruggeri <DRuggeri@primary.net>
2025-05-24 19:35:31 -0500

    Send brief status to statusinator and reopen on failed port writes

--- statusinator/statusinator.go
+++ statusinator/statusinator.go
@@ -8,6 +8,8 @@ import (
 	"log/slog"
 	"sync"
 
+	"github.com/DRuggeri/labwatch/powerman"
+	"github.com/DRuggeri/labwatch/talosinitializer"
 	"github.com/DRuggeri/labwatch/watchers/loki"
 	"github.com/jacobsa/go-serial/serial"
 )
@@ -17,6 +19,13 @@ type Statusinator struct {
 	port io.ReadWriteCloser
 	log  *slog.Logger
 	mux  *sync.Mutex
+}
+
+// Cuts out Talos and port monitoring information to keep payload less than 2048 bytes
+type BriefStatus struct {
+	Initializer talosinitializer.InitializerStatus `json:"initializer"`
+	Power       powerman.PowerStatus               `json:"power"`
+	Logs        loki.LogStats                      `json:"logs"`
 }
 
 func NewStatusinator(port string, l *slog.Logger) (*Statusinator, error) {
@@ -54,7 +63,11 @@ func (m *Statusinator) Watch(controlContext context.Context, status <-chan LabSt
 		case s, ok := <-status:
 			if ok {
 				m.log.Debug("received status update")
-				b, err := json.Marshal(s)
+				b, err := json.Marshal(BriefStatus{
+					Power:       s.Power,
+					Logs:        s.Logs,
+					Initializer: s.Initializer,
+				})
 				if err != nil {
 					m.log.Error("failed to marshal to JSON", "object", b)
 					return
@@ -87,5 +100,14 @@ func (m *Statusinator) send(t string, payload []byte) {
 	b = append(b, payload...)
 	b = append(b, '\n')
 
-	m.port.Write(b)
+	_, err := m.port.Write(b)
+	if err != nil {
+		p, oerr := serial.Open(m.opts)
+		if oerr != nil {
+			m.log.Warn("error writing to port - failed to reopen", "error", err, "openError", oerr)
+			return
+		}
+		m.log.Warn("error writing to port - reopened", "error", err)
+		m.port = p
+	}
 }
